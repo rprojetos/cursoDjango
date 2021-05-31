41ff87d0f63117c52b749373a82e08bdf1d32454
rprojetos <rprojetos.ti@hotmail.com>
2021-05-30 22:19:10 -0300

    Aula sobre sistema de templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    lista = [
+        'Django',
+        'Python',
+        'Git',
+        'Html',
+        'Banco de dados',
+        'Linux',
+        'Nginx',
+        'Uwsgi',
+        'Systemctl'
+    ]
+    data = {'name':'Curso de DJango 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
